ccc1f0a9409a3356a995815068915645155a56ae
amir <42087527+AmirMansurian@users.noreply.github.com>
2022-06-21 23:10:54 -0700

    Update args.py

--- args.py
+++ args.py
@@ -27,7 +27,7 @@ def get_arguments():
         "--batch-size",
         "-b",
         type=int,
-        default=8,
+        default=6,
         help="The batch size. Default: 10")
     parser.add_argument(
         "--epochs",
