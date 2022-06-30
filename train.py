90ec91cc276c91c7fd759c69803b04c83d311ab9
amir <42087527+AmirMansurian@users.noreply.github.com>
2022-06-30 22:21:31 +0800

    Update train.py

--- train.py
+++ train.py
@@ -123,6 +123,26 @@ class Train:
         self.model.train()
         self.teacher.eval()
 
+        #criteria_pre = OhemCELoss(0.7)
+        #criteria_aux = [OhemCELoss(0.7) for _ in range(4)]
+
+        #optim = set_optimizer(self.model)
+
+        #self.teacher = utils.load_checkpoint(self.teacher, self.optim, 'save/', 'BiSeNet2v170')[0]
+
+        ## mixed precision training
+        #scaler = amp.GradScaler()
+
+
+        ## meters
+        #time_meter, loss_meter, loss_pre_meter, loss_aux_meters = set_meters()
+
+        ### lr scheduler
+        #lr_schdr = WarmupPolyLrScheduler(optim, power=0.9,
+         #   max_iter=150000, warmup_iter=1000,
+         #   warmup_ratio=0.1, warmup='exp', last_epoch=-1,)
+
+
 
         epoch_loss = 0.0
         self.metric.reset()
@@ -130,39 +150,114 @@ class Train:
             # Get the inputs and labels
             inputs = batch_data[0].to(self.device)
             labels = batch_data[1].to(self.device)
-            
 
-            [outputs, feature_maps] = self.model(inputs)
+            # Forward propagation
+           # outputs = self.model(inputs)[0]
+            
+           
+
+            ########################### teacher output ###########################################
+            #with torch.no_grad():
+            #  teacher_out = self.teacher(inputs)
+            
+            #distill_loss = nn.KLDivLoss()(F.log_softmax(outputs, dim=1), F.softmax(teacher_out, dim=1))
+            ######################################################################################
+
+           # print(outputs)
+            #print(teacher_out)
+
+            #[outputs, feature_maps] = self.model(inputs)
+            outputs = self.model(inputs)
 
             with torch.no_grad():
               [teacher_out, T_f] = self.teacher(inputs)
 
-            T_f.detach()
+            #T_f.detach()
+             # print(T_f.shape)
 
             self.optim.zero_grad()
 
+            #print(teacher_out[0, :, 0, 0])
+            #print(outputs[0, :, 0, 0])
+            #print(labels[0, 0, 0])
+
+
+           # a = F.softmax(outputs, dim=1)
+           # b = F.softmax(teacher_out, dim=1)
+
+            #print(b[0, :, 0, 0])
+            #print(a[0, :, 0, 0])
+            #print(labels[0, 0, 0])
+
+
+            #print(teacher_out.shape)
+            #print(outputs.shape)
+            #print(labels.shape)
+
+            #loss = self.criterion(outputs, labels)
+
+
+
+
+
+
+
+            #optim.zero_grad()
+           # with amp.autocast(enabled=True):
+             #   logits, *logits_aux = self.model(inputs)
+               # loss_pre = criteria_pre(logits, labels)
+               # loss_aux = [crit(lgt, labels) for crit, lgt in zip(criteria_aux, logits_aux)]
+               # loss = loss_pre + sum(loss_aux)
+           # scaler.scale(loss).backward()
+            #scaler.step(optim)
+            #scaler.update()
+            #torch.cuda.synchronize()
+
+            #time_meter.update()
+            #loss_meter.update(loss.item())
+            #loss_pre_meter.update(loss_pre.item())
+            #_ = [mter.update(lss.item()) for mter, lss in zip(loss_aux_meters, loss_aux)]
+
+
             ##########################################
-           # T = 1
-           # distill_loss = 10 * nn.KLDivLoss()(F.log_softmax(outputs/T, dim=1), F.softmax(teacher_out/T, dim=1))
-           #### loss = self.criterion(outputs, labels) + 10 * distill_loss 
+            T = 1
+            distill_loss = nn.KLDivLoss()(F.log_softmax(outputs/T, dim=1), F.softmax(teacher_out/T, dim=1))
+            loss = self.criterion(outputs, labels) + 10 * distill_loss 
             #########################################
             
+          
+            #TF = torch.mean(T_f, axis=1)
+            #SF = torch.mean(feature_maps, axis=1)
 
-            TF = T_f
-            SF = feature_maps
-
+            ##TF = T_f
+            #SF = feature_maps
+            #print(TF.shape)
+            #print(SF.shape)
             loss = 0.0
+
             temp = self.criterion(outputs, labels)
             self.loss = temp.item()
             loss = loss + temp
 
-            temp2 = loss_function(TF, SF)
-            self.distillation_loss = temp2.item()
-            loss = loss + temp2
+            #temp2 = loss_function(TF, SF)
+            #self.distillation_loss = temp2.item()
+            #loss = loss + temp2
 
-           # loss = loss + distill_loss
+            #loss = loss + distill_loss
            # print(outputs.shape)
             
+            #print(los(TF, SF))
+            #loss = loss + temp
+            #print(loss)
+            #print(labels.shape)
+            #print(outputs.shape)
+            
+            #loss = self.criterion(outputs, labels)
+
+            #print(self.criterion(outputs, labels))
+            #print(distill_loss)
+
+              # Backpropagation
             
             loss.backward()
             self.optim.step()
@@ -172,11 +267,44 @@ class Train:
 
 
 
+
+
+
+            # Loss computation
+            #loss = self.criterion(outputs, labels)
+            #print('#####################' + str(loss)+'###################')
+            #################### changed #########################
+            #loss = 10*distill_loss
+            #print('#####################' + str(loss2)+'###################')
+            ######################################################
+
+
+            # Backpropagation
+           # self.optim.zero_grad()
+            #loss.backward()
+            #self.optim.step()
+
+            # Keep track of loss for current epoch
+            #epoch_loss += loss.item()
+
+            # Keep track of the evaluation metric
+            #self.metric.add(logits.detach(), labels.detach())
+
+            #print(outputs.shape)
+            #print(labels.shape)
+            #loss = 1* self.criterion(outputs, labels)
+            #loss += 0.4 * nn.KLDivLoss()(F.log_softmax(outputs, dim=1), F.softmax(teacher_out, dim=1))
+
+          
+
             # Keep track of the evaluation metric
             self.metric.add(outputs.detach(), labels.detach())
 
             if iteration_loss:
                     print("[Step: %d] Iteration loss: %.4f" % (step, loss.item()))
+
+        #return epoch_loss / len(self.data_loader), self.metric.value()  
           
 
         return epoch_loss / len(self.data_loader), self.metric.value() 
+       # return loss / len(self.data_loader), self.metric.value()
